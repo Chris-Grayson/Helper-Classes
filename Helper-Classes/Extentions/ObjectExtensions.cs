2b6098c25e908e6ec3750f27b53d8bb52373982d
Chris-Grayson <chris.p.grayson@outlook.com>
2016-05-04 13:55:58 +0100

    ObjectExtension added

---
+++ Helper-Classes/Extentions/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Helper_Classes.Extentions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Throws an ArgumentNullException if the object is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">The object.</param>
+        /// <param name="paramName">Name of the parameter.</param>
+        /// <param name="message">The message.</param>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        public static void ThrowIfNull<T>(this T obj, string paramName, string message = null)
+        {
+            if (obj.Equals(null))
+                throw new ArgumentNullException(paramName, message ?? "Value cannot be null.");
+        }
+    }
+}
